ab1176b62ad42ddc13281e519511f1229aa50747
danila-k-open <156195550+danila-k-open@users.noreply.github.com>
2024-02-21 12:46:32 +0500

    Update script.js
    Обновление базовых функций js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,49 @@
-console.log('hello from script.js!');
+(function($) {
+    $(document).ready(function () {
+        $(window).scroll(function() {
+            if($(this).scrollTop() > 100) {
+                $('#scrollToTop').fadeIn();
+            } else {
+                $('#scrollToTop').fadeOut();
+            }
+        });
+
+        $('#scrollToTop').click(function() {
+            $('html, body').animate({scrollTop:0}, 'slow');
+        });
+        // $('.menu.sf-menu.sf-main li a').click(function() {
+        //     $('html, body').animate({scrollTop:$($(this).attr('href')).offset().top}, 'slow');
+        // }); плавный скролл до якоря по клику на пункт меню
+
+
+        $("#mobile-nav").mmenu({
+            extensions: ["pagedim-black"],
+            navbar: {
+                title: "Главное меню",
+            },
+        });
+        $('a.mm-next').click(function() {
+            if($('.mm-panel').hasClass('mm-opened') && $('.mm-panel > ul').hasClass('sf-hidden')){
+                $('.mm-panel > ul').removeClass('sf-hidden');
+            }
+        });
+        $('a.mm-btn.mm-prev').click(function() {
+            if($('.mm-panel').hasClass('mm-opened') && !$('.mm-panel > ul').hasClass('sf-hidden')){
+                $('.mm-panel > ul').addClass('sf-hidden');
+                $('.mm-panel').removeClass('mm-opened');
+            }
+        });
+
+    });
+    // $(document).ajaxComplete(function(event, jqXHR, settings) {
+    //     var jsonlog = {
+    //         'URL запроса:': settings.url,
+    //         'Метод запроса:': settings.method,
+    //         'Данные запроса:': settings.data,
+    //         'Статус запроса:': jqXHR.status,
+    //         'Ответ сервера': jqXHR.responseJSON,
+    //     };
+    //     //console.log(jsonlog);
+    // });
+})(jQuery);
+
